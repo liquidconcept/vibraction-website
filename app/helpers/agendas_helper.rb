85bb5d49282b4f63b8d477e8b3eff1dc2b1612f2
Ludovic Turmel <ludovic.turmel@liquid-concept.ch>
2014-09-23 14:42:09 +0200

    Display correctly date of crouse

--- app/helpers/agendas_helper.rb
+++ app/helpers/agendas_helper.rb
@@ -1,9 +1,15 @@
 module AgendasHelper
   def date_of_course(identifier)
     Agenda.date_of_course(identifier).map do |group, dates|
-      content_tag(:div, class: 'course') do
-        content_tag(:ul, dates_html(dates)) +
-        content_tag(:a, 'inscription', class: "submit #{dates.first.full}" , href: "./inscription.html?cours_name=#{dates.first.name}&cours_date=#{dates.first.start_at}")
+      if dates.last.start_at.to_date > Date.today
+        button_class = ''
+        button_class << ' started' if dates.first.start_at.to_date < Date.today
+        button_class << ' full' if dates.first.full
+
+        content_tag(:div, class: 'course') do
+          content_tag(:ul, dates_html(dates)) +
+          content_tag(:a, 'inscription', class: "submit #{button_class}" , href: "./inscription.html?cours_name=#{dates.first.name}&cours_date=#{dates.first.start_at}")
+        end
       end
     end.join.html_safe
   end
