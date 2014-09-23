fd73f695b35acadcd04ff42023816c1559a82428
Ludovic Turmel <ludovic.turmel@liquid-concept.ch>
2014-09-23 14:43:38 +0200

    Fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,30 +14,20 @@ $(function() {
   // Lightbox page tharapies
   $('#gallery a').lightBox(); // Select all links in object with gallery ID
 
-   if ($('a.submit').hasClass('true')) {
-    $('a.true').addClass('full');
-    $('a.true').css('cursor', 'default');
-   }
-
-   $('a.submit.full').attr('href', '#', function() {
-      preventDefault();
-   });
-
-  // Toggle content cours
-  $('a.submit:not(.full)').bind('click', function(e) {
-    e.stopImmediatePropagation();
-    });
-
-  $('a.submit.full').bind('click', function(e) {
+  // submit button
+  $('a.submit.full').on('click', function(e) {
     e.preventDefault();
     e.stopImmediatePropagation();
-    });
+  });
 
+  // Toggle content cours
   $('.more').hide();
-
   $('.course-box').bind('mouseenter', function() {
     $(this).find('.more:hidden').slideDown();
-    }).bind('click', function() {
-      $(this).find('.more:visible').slideUp();
-    });
+  }).bind('click', function() {
+    $(this).find('.more:visible').slideUp();
+  });
+
+  // display off button if cours as started
+  $('a.started').hide();
 });
--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140916150624) do
+ActiveRecord::Schema.define(version: 20140923082634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "agendas", force: true do |t|
-    t.string  "name"
-    t.integer "identifier"
-    t.integer "group"
-    t.time    "start_at"
-    t.time    "end_at"
-    t.integer "session"
-    t.boolean "full"
+    t.string   "name"
+    t.integer  "identifier"
+    t.integer  "group"
+    t.integer  "session"
+    t.boolean  "full"
+    t.datetime "start_at"
+    t.datetime "end_at"
   end
 
   create_table "users", force: true do |t|
