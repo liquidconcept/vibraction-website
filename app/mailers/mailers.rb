3d665727d8e3bf152e4a7bf0b8f89a1bbb39894e
Ludovic Turmel <ludovic.turmel@liquid-concept.ch>
2014-10-13 13:43:20 +0200

    Change mail to for mailer

--- app/mailers/mailers.rb
+++ app/mailers/mailers.rb
@@ -4,7 +4,7 @@ class Mailers < ActionMailer::Base
 
   def contact(contact)
     @contact = contact
-    mail(to: 'ludovic.turmel@gmail.com', subject: "Vibraction Website contact", from: @contact.email) do |format|
+    mail(to: 'info@vibraction.ch', subject: "Vibraction Website contact", from: @contact.email) do |format|
        format.html { render 'mailers/contact' }
     end
   end
